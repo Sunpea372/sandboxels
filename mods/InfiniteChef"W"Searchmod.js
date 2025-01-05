bd8c4cd43c26fb65e39dbdbd2beb6cc80578e449
Sunpea372 <sunpea372@gmail.com>
2025-01-05 21:51:01 +0200

    Create InfiniteChef"W"Searchmod.js
    A Sandboxels mod adding Infinite Chef "W" search ingredients

---
+++ mods/InfiniteChef"W"Searchmod.js
@@ -0,0 +1,24 @@
+elements.walnut = {
+    color: ["#B7833A", "#DCA354"],
+    behavior: behaviors.POWDER,
+    category: "food",
+    state: "solid",
+    breakInto: "nut_meat"
+}
+elements.watercress = {
+    color: "#0FCA1B",
+    behavior: behaviors.POWDER,
+    category: "life",
+    state: "solid",
+    burn: 37.5,
+    burnTime: 900,
+    burnInto: ["steam", "smoke", "fire", "fire",],
+    tempHigh: 111,
+}
+elements.wasabiroot = {
+    color: "#556F32",
+    behavior: behaviors.POWDER,
+    category: "life",
+    state: "solid",
+    breakInto: "wasabi"
+}
