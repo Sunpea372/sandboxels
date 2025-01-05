3e47829f0c46d752a6119cac554cf2e7581f7895
Sunpea372 <sunpea372@gmail.com>
2025-01-05 19:40:08 +0200

    Create infinitechefQingredientmod.js
    A Sandboxels mod, adding Infinite Chef ingredients from the "Q" search

---
+++ mods/infinitechefQingredientmod.js
@@ -0,0 +1,46 @@
+elements.quince = {
+    behavior: behaviors.POWDER,
+    category: "food",
+    state: "solid",
+    color: "#C1B81B",
+    breakInto: "juice",
+    breakIntoColor: "#C1B81B",
+    reactions: {},
+  };
+elements.quail.egg = {
+    behavior: behaviors.POWDER,
+    category: "food",
+    state: "solid",
+    color: "#9B9E73",
+    breakInto: "yolk",
+    reactions: {},
+};
+elements.squash = {
+    behavior: behaviors.POWDER,
+    category: "food",
+    state: "solid",
+    color: "#eeb414",
+    breakInto: "juice",
+    reactions: {},
+};
+elements.kumquat = {
+    behavior: behaviors.POWDER,
+    category: "food",
+    state: "solid",
+    color: "#F3B86A,
+    reactions: {},
+};
+elements.acorn.squash = {
+    behavior: behaviors.POWDER,
+    category: "food",
+    state: "solid",
+    color: "#2A3129",
+    reactions: {},
+};
+elements.licorice.root = {
+    behavior: behaviors.POWDER,
+    category: "life",
+    state: "solid",
+    color: "#B59A89",
+    reactions: {},
+  
